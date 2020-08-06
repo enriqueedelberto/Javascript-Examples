748617a6567063a3e741cf951c3c64f2d6997c64
enriqueedelberto <enriqueviiireyes@gmail.com>
2020-08-05 22:43:47 -0500

    Tetris works

--- FreeCodeCamp_JavascriptBySevenGames/Tetris/app.js
+++ FreeCodeCamp_JavascriptBySevenGames/Tetris/app.js
@@ -22,13 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //assign functions to keycodes
     function control(e) {
-        if (e.KeyCode === RIGHT_DIRECCION_KEY) {
+        if (e.keyCode === RIGHT_DIRECCION_KEY) {
             moveRight();
-        } else if (e.KeyCode === UP_DIRECCION_KEY) {
+        } else if (e.keyCode === UP_DIRECCION_KEY) {
             rotate();
-        } else if (e.KeyCode === LEFT_DIRECCION_KEY) {
+        } else if (e.keyCode === LEFT_DIRECCION_KEY) {
             moveLeft();
-        } else if (e.KeyCode === DOWN_DIRECCION_KEY) {
+        } else if (e.keyCode === DOWN_DIRECCION_KEY) {
             moveDown();
         }
     }
@@ -44,31 +44,31 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     const zTetromino = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
     ];
 
     const tTetromino = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2],
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1],
     ];
 
     const oTetromino = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
     ];
 
     const iTetromino = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
     ];
 
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
@@ -98,8 +98,6 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPosition = currentPosition += width;
         draw();
         freeze();
-
-
     }
 
 
@@ -110,7 +108,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
         if (!isAtRightEdge) {
             currentPosition += 1;
-
         }
 
         if (current.some(index => squares[currentPosition + index].classList.contains("block2"))) {
@@ -151,7 +148,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayIndex = 0;
 
     const smallTetrominoes = [
-        [1, displayWidth + 1, displayWidth * 2, 2], //lTeromino 
+        [1, displayWidth + 1, displayWidth * 2 + 1, 2], //lTeromino 
         [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //zTeromino 
         [1, displayWidth, displayWidth + 1, displayWidth + 2], //tTeromino 
         [0, 1, displayWidth, displayWidth + 1], //oTeromino 
@@ -168,15 +165,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-
-
+    //freeze the shape
     function freeze() {
         if (current.some(index => squares[currentPosition + index + width].classList.contains("block3") ||
                 squares[currentPosition + index + width].classList.contains("block2"))) {
             current.forEach(index => squares[index + currentPosition].classList.add("block2"));
             random = nextRandom;
             nextRandom = Math.floor(Math.random() * theTetrominoes.length);
-            current = theTetrominoes[random][currentPosition];
+            current = theTetrominoes[random][currentRotation];
             currentPosition = 4;
             draw();
             displayShape();
@@ -216,14 +212,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 lines += 1;
                 scoreDisplay.innerHTML = score;
                 linesDisplay.innerHTML = lines;
-                row.forEach(inde => {
-                    squares[index].classList.remove("block2") || squares[index].classList.remove("block");
+                row.forEach(index => {
+                    squares[index].classList.remove("block2") || squares[index].classList.remove("block")
                 });
 
                 //splice array
                 const squaresRemoved = squares.splice(currentIndex, width);
                 squares = squaresRemoved.concat(squares);
-                squares.forEach(cell = grid.appendChild(cell));
+                squares.forEach(cell => grid.appendChild(cell));
 
             }
 
